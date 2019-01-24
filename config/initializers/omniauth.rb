f62178787335393bfd879dc13a57f7adc3b01c0e
Jan Hovancik <conta.srdr@gmail.com>
2019-01-24 18:56:59 +0100

    Get less data from Patreon

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :patreon, ENV['PATREON_CLIENT_ID'], ENV['PATREON_CLIENT_SECRET'], path_prefix: '/app/auth'
+  provider :patreon, ENV['PATREON_CLIENT_ID'], ENV['PATREON_CLIENT_SECRET'],
+    path_prefix: '/app/auth', scope: 'users'
 end
