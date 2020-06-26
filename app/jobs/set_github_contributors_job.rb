1f37667a8b2fc19b1b54de8d9310577f5c2f432b
Jan Hovancik <conta.srdr@gmail.com>
2020-06-26 18:16:18 +0200

    Allows GH sponsors to login as well

--- app/jobs/set_github_contributors_job.rb
+++ app/jobs/set_github_contributors_job.rb
@@ -1,7 +1,8 @@
 # Check for Github contributors
 class SetGithubContributorsJob < ApplicationJob
   def perform
-    contributor_ids = Github::GetContributorsService.perform
+    contributor_ids = (Github::GetContributorsService.perform +
+      Github::GetSponsorsService.perform).uniq
     service = Users::SetGithubContributorsService.new(contributor_ids)
     service.perform
   end
