97b89662025e68cb4d49493c2a18b1ac85e3242e
Jan Hovancik <conta.srdr@gmail.com>
2020-07-30 21:48:56 +0200

    Use less scope for GH
    https://github.com/hovancik/stretchly/issues/647

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :patreon, ENV['PATREON_CLIENT_ID'], ENV['PATREON_CLIENT_SECRET'],
     path_prefix: '/app/v1/auth', scope: 'users'
   provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'],
-    path_prefix: '/app/auth', scope: 'user'
+    path_prefix: '/app/auth', scope: 'read:user'
   provider :github, ENV['GITHUB_CLIENT_ID_V1'], ENV['GITHUB_CLIENT_SECRET_V1'],
-    path_prefix: '/app/v1/auth', scope: 'user'
+    path_prefix: '/app/v1/auth', scope: 'read:user'
 end
