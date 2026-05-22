882c4dcb2747f998d69773bce369ccda0d0b1a2a
Jan Hovancik <jan@hovancik.net>
2026-05-22 13:25:52 +0000

    Upgrade Rails to 8.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2023_12_02_103043) do
+ActiveRecord::Schema[8.1].define(version: 2023_12_02_103043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "delayed_jobs", force: :cascade do |t|
-    t.integer "priority", default: 0, null: false
     t.integer "attempts", default: 0, null: false
+    t.datetime "created_at", precision: nil
+    t.datetime "failed_at", precision: nil
     t.text "handler", null: false
     t.text "last_error"
-    t.datetime "run_at", precision: nil
     t.datetime "locked_at", precision: nil
-    t.datetime "failed_at", precision: nil
     t.string "locked_by"
+    t.integer "priority", default: 0, null: false
     t.string "queue"
-    t.datetime "created_at", precision: nil
+    t.datetime "run_at", precision: nil
     t.datetime "updated_at", precision: nil
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "settings", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.jsonb "data", default: "{}", null: false
     t.datetime "created_at", null: false
+    t.jsonb "data", default: "{}", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_settings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "provider", null: false
-    t.string "uid", null: false
     t.string "auth_token", null: false
     t.boolean "contributor", default: false, null: false
     t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
     t.boolean "manual_contributor", default: false, null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["auth_token"], name: "index_users_on_auth_token"
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["provider"], name: "index_users_on_provider"
