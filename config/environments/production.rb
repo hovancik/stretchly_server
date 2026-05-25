7c674bf12d8aa80c8d39a1f0f2106ae38b8fc933
Jan Hovancik <jan@hovancik.net>
2026-05-25 06:20:56 +0000

    Unused

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,9 +40,6 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
-
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
